bce468834b06d0c40663af0d0175e06306cead33
Benjamin Kober <kober@optisense.com>
2022-06-13 20:01:27 +0200

    task: added nix shell for reproducability

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ profile ? "minimal" }:
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { overlays = [ (import sources.rust-overlay) ]; };
+in
+pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.rust-bin.stable.latest.${profile}
+
+    # keep this line if you use bash
+    pkgs.bashInteractive
+  ];
+}
+
