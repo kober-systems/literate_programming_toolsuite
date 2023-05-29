481bdc31700b716a62c60391f44ee6d85114ca8f
Benjamin Kober <benko@kober-systems.com>
2023-05-29 13:44:09 +0200

    task: added test for sourcecode blocks

--- asciidoctrine/tests/asciidoc_to_html_test.rs
+++ asciidoctrine/tests/asciidoc_to_html_test.rs
@@ -74,3 +74,33 @@ This Information is visible by default.
   Ok(())
 }
 
+#[test]
+fn sourcecode_blocks() -> Result<()> {
+  let content = r#"
+[source, bash]
+----
+echo "hello world!"
+----
+"#;
+  let reader = AsciidocReader::new();
+  let mut opts = options::Opts::parse_from(vec!["--template", "-"].into_iter());
+  opts.template = Some("-".into());
+  let mut env = util::Env::Cache(util::Cache::new());
+  let ast = reader.parse(content, &opts, &mut env)?;
+
+  let mut buf = BufWriter::new(Vec::new());
+  let mut writer = HtmlWriter::new();
+  writer.write(ast, &opts, &mut buf)?;
+
+  let output = String::from_utf8(buf.into_inner()?)?;
+  assert_eq!(
+    output,
+    r#"<div class="listingblock">
+  <pre>echo "hello world!"</pre>
+</div>
+"#
+  );
+
+  Ok(())
+}
+
