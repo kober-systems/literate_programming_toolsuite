9fedcf758236dd15ab648e7f6d03342c2dc474b1
Benjamin Kober <benko@kober-systems.com>
2023-11-30 13:22:37 +0100

    asciidoctrine: generalized cli handling

---
+++ asciidoctrine/src/cli_template.rs
@@ -0,0 +1,64 @@
+use anyhow::{bail, Context, Result};
+use crate::*;
+use std::fs;
+use std::io::{self, Read, Write};
+
+pub fn cli_template(
+  handle_extensions: for<'a> fn(opts: &'a options::Opts, ast: AST<'a>) -> AST<'a>,
+) -> Result<()> {
+  simple_logger::init()?;
+  let opts = options::from_args();
+
+  let reader: Box<dyn Reader> = match opts.readerfmt {
+    options::Reader::Asciidoc => Box::new(AsciidocReader::new()),
+    options::Reader::Json => Box::new(JsonReader::new()),
+  };
+
+  // read the input
+  let input = match &opts.input {
+    Some(input) => fs::read_to_string(input).context("Could not read in file")?,
+    None => {
+      let mut input = String::new();
+      io::stdin()
+        .read_to_string(&mut input)
+        .context("Could not read stdin")?;
+      input
+    }
+  };
+
+  let mut env = util::Env::Io(util::Io::new());
+  let ast = reader.parse(&input, &opts, &mut env)?;
+
+  let ast = handle_extensions(&opts, ast);
+
+  let output: Box<dyn Write> = match &opts.output {
+    Some(output) => Box::new(fs::File::create(output).context("Could not open output file")?),
+    None => Box::new(io::stdout()),
+  };
+
+  match opts.writerfmt {
+    options::Writer::Html5 => HtmlWriter::new().write(ast, &opts, output)?,
+    options::Writer::Json => JsonWriter::new().write(ast, &opts, output)?,
+    options::Writer::Docx => match &opts.output {
+      Some(output) => {
+        DocxWriter::new().write(
+          ast,
+          &opts,
+          fs::File::create(output).context("Could not open output file")?,
+        )?;
+      }
+      None => bail!("docx cant only be written to file not to stdout"),
+    },
+    _ => bail!("not yet supported"),
+  };
+
+  Ok(())
+}
+
+pub fn cli_no_extensions() -> Result<()> {
+  cli_template(no_extensions)
+}
+
+fn no_extensions<'a>(_opts: &'a options::Opts, ast: AST<'a>) -> AST<'a> {
+  ast
+}
--- asciidoctrine/src/main.rs
+++ asciidoctrine/src/main.rs
@@ -1,45 +1,6 @@
-extern crate asciidoctrine;
-
-use anyhow::{Context, Result};
+use anyhow::Result;
 use asciidoctrine::*;
-use std::fs;
-use std::io::{self, Read};
 
 fn main() -> Result<()> {
-  let opts = options::from_args();
-
-  let reader: Box<dyn Reader> = match opts.readerfmt {
-    options::Reader::Asciidoc => Box::new(AsciidocReader::new()),
-    options::Reader::Json => Box::new(JsonReader::new()),
-  };
-
-  // read the input
-  let input = match &opts.input {
-    Some(input) => fs::read_to_string(input).context("Could not read in file")?,
-    None => {
-      let mut input = String::new();
-      io::stdin()
-        .read_to_string(&mut input)
-        .context("Could not read stdin")?;
-      input
-    }
-  };
-
-  let mut env = util::Env::Io(util::Io::new());
-  let ast = reader.parse(&input, &opts, &mut env)?;
-
-  // TODO Wenn Erweiterungen in den Kommandozeilenparametern angegeben sind
-  // diese in einer Schleife den AST manipulieren lassen
-  // TODO Es sollte zwei Arten von Erweiterungen geben:
-  // * Die ersten sind Kommandozeilenprogramme, die auf stdin Json bekommen und auf Stdout
-  //   wieder Json ausgeben. Diese sollten auf der Kommandozwile parameter übergeben bkommen
-  //   können.
-  // * Die zweiten sind (lua)-Scripte, die den AST als Struktur übergeben bekommen und wieder
-  //   einen AST zurückgeben.
-
-  // TODO Das Ausgabeformat festlegen
-  //println!("{:#?}", opts);
-  //println!("\n\n{:#?}", ast);
-
-  Ok(())
+  cli_template::cli_no_extensions()
 }
