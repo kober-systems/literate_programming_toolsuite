4c06f545c53788d1bd11c0e4c327c8bf9a19ba6d
Benjamin Kober <benko@kober-systems.com>
2025-11-10 13:13:11 +0200

    indicate if a block is closed

--- ansicht/src/reader/ascii_art.rs
+++ ansicht/src/reader/ascii_art.rs
@@ -252,7 +252,10 @@ impl PartialElement {
     }
   }
 
-  fn add_token(&mut self, token: Token) {
+  /// Add a token to the partial element
+  ///
+  /// return: is the elment closed by this token
+  fn add_token(&mut self, token: Token) -> bool {
     let BoundingBox { start: _, end } = token.get_bounds();
     self.tokens.push(token);
     if end.line >= self.clock_cycle_end.line && end.column >= self.clock_cycle_end.column {
@@ -260,6 +263,7 @@ impl PartialElement {
     } else {
       self.counter_clock_cycle_end = end;
     }
+    false
   }
 }
 
@@ -973,7 +977,7 @@ mod tests {
       started_block.can_continue_block(&next_token, SINGLE_BOX),
       true
     );
-    started_block.add_token(next_token);
+    assert_eq!(started_block.add_token(next_token), false);
 
     // ConnectionSign
     let next_token = tokens.pop().unwrap();
@@ -981,7 +985,7 @@ mod tests {
       started_block.can_continue_block(&next_token, SINGLE_BOX),
       true
     );
-    started_block.add_token(next_token);
+    assert_eq!(started_block.add_token(next_token), false);
     assert_eq!(started_block.clock_cycle_end.line, 2);
     assert_eq!(started_block.clock_cycle_end.column, 10);
     assert_eq!(started_block.counter_clock_cycle_end.line, 2);
@@ -993,7 +997,7 @@ mod tests {
       started_block.can_continue_block(&next_token, SINGLE_BOX),
       true
     );
-    started_block.add_token(next_token);
+    assert_eq!(started_block.add_token(next_token), false);
     assert_eq!(started_block.clock_cycle_end.line, 2);
     assert_eq!(started_block.clock_cycle_end.column, 10);
     assert_eq!(started_block.counter_clock_cycle_end.line, 3);
@@ -1012,7 +1016,7 @@ mod tests {
       started_block.can_continue_block(&next_token, SINGLE_BOX),
       true
     );
-    started_block.add_token(next_token);
+    assert_eq!(started_block.add_token(next_token), false);
     assert_eq!(started_block.clock_cycle_end.line, 3);
     assert_eq!(started_block.clock_cycle_end.column, 10);
     assert_eq!(started_block.counter_clock_cycle_end.line, 3);
@@ -1024,7 +1028,7 @@ mod tests {
       started_block.can_continue_block(&next_token, SINGLE_BOX),
       true
     );
-    started_block.add_token(next_token);
+    assert_eq!(started_block.add_token(next_token), false);
     assert_eq!(started_block.clock_cycle_end.line, 3);
     assert_eq!(started_block.clock_cycle_end.column, 10);
     assert_eq!(started_block.counter_clock_cycle_end.line, 4);
@@ -1036,7 +1040,7 @@ mod tests {
       started_block.can_continue_block(&next_token, SINGLE_BOX),
       true
     );
-    started_block.add_token(next_token);
+    assert_eq!(started_block.add_token(next_token), false);
     assert_eq!(started_block.clock_cycle_end.line, 3);
     assert_eq!(started_block.clock_cycle_end.column, 10);
     assert_eq!(started_block.counter_clock_cycle_end.line, 4);
