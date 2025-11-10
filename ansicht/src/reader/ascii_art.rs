29f72a482c6be503c4f158729d72c8116ee2e7ab
Benjamin Kober <benko@kober-systems.com>
2025-11-10 12:39:30 +0200

    continue parsing blocks

--- ansicht/src/reader/ascii_art.rs
+++ ansicht/src/reader/ascii_art.rs
@@ -199,9 +199,13 @@ impl PartialElement {
       HLine {
         line,
         column_start,
-        column_end,
+        column_end: _,
       } => {
         if self.clock_cycle_end.line == *line && self.clock_cycle_end.column + 1 == *column_start {
+          true
+        } else if self.counter_clock_cycle_end.line == *line
+          && self.counter_clock_cycle_end.column + 1 == *column_start
+        {
           true
         } else {
           false
@@ -1019,6 +1023,18 @@ mod tests {
     assert_eq!(started_block.clock_cycle_end.column, 10);
     assert_eq!(started_block.counter_clock_cycle_end.line, 4);
     assert_eq!(started_block.counter_clock_cycle_end.column, 4);
+
+    // HLine
+    let next_token = tokens.pop().unwrap();
+    assert_eq!(
+      started_block.can_continue_block(&next_token, SINGLE_BOX),
+      true
+    );
+    started_block.add_token(next_token);
+    assert_eq!(started_block.clock_cycle_end.line, 3);
+    assert_eq!(started_block.clock_cycle_end.column, 10);
+    assert_eq!(started_block.counter_clock_cycle_end.line, 4);
+    assert_eq!(started_block.counter_clock_cycle_end.column, 9);
   }
 
   const SINGLE_BOX: &str = r"
