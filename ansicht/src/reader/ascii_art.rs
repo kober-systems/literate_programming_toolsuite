15207719049b36dfd016fc61144e2d0ed295c64a
Benjamin Kober <benko@kober-systems.com>
2025-11-10 12:32:32 +0200

    continue parsing blocks

--- ansicht/src/reader/ascii_art.rs
+++ ansicht/src/reader/ascii_art.rs
@@ -210,6 +210,10 @@ impl PartialElement {
       ConnectionSign { line, column } => {
         if self.clock_cycle_end.line == *line && self.clock_cycle_end.column + 1 == *column {
           true
+        } else if self.counter_clock_cycle_end.line + 1 == *line
+          && self.counter_clock_cycle_end.column == *column
+        {
+          true
         } else {
           false
         }
@@ -1002,6 +1006,18 @@ mod tests {
     assert_eq!(started_block.clock_cycle_end.line, 3);
     assert_eq!(started_block.clock_cycle_end.column, 10);
     assert_eq!(started_block.counter_clock_cycle_end.line, 3);
+    assert_eq!(started_block.counter_clock_cycle_end.column, 4);
+
+    // ConnectionSign
+    let next_token = tokens.pop().unwrap();
+    assert_eq!(
+      started_block.can_continue_block(&next_token, SINGLE_BOX),
+      true
+    );
+    started_block.add_token(next_token);
+    assert_eq!(started_block.clock_cycle_end.line, 3);
+    assert_eq!(started_block.clock_cycle_end.column, 10);
+    assert_eq!(started_block.counter_clock_cycle_end.line, 4);
     assert_eq!(started_block.counter_clock_cycle_end.column, 4);
   }
 
